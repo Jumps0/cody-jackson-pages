55ade523c3bc1f43b8ac76a3949a0e62b143ee41
Jumps0 <49619518+Jumps0@users.noreply.github.com>
2026-08-10 10:33:47 +0200

    Basic react website code upload

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <section id="center">
+        <h1>Welcome</h1>
+        <h2>Let's get started.</h2>
+      </section>
+    </>
+  )
+}
+
+export default App

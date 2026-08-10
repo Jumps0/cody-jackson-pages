d976957a07db76c010d37b7ccac6b35993ff205d
Jumps0 <49619518+Jumps0@users.noreply.github.com>
2026-08-10 12:57:21 +0200

    basic header / contact info setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,42 @@
-import './App.css'
+﻿import './App.css'
 
 function App() {
   return (
-    <>
-      <section id="center">
-        <h1>Welcome</h1>
-        <h2>Let's get started.</h2>
+    <main className="app-shell">
+      <section className="title-section">
+        <aside className="contact-card">
+          <h3>Contact</h3>
+          <dl>
+            <div>
+              <dt>Address</dt>
+              <dd>Tankedraget 3, 2. 1 <br/> 9000 Aalborg, Denmark</dd>
+            </div>
+            <div>
+              <dt>Phone</dt>
+              <dd>+45 25 12 29 23</dd>
+            </div>
+            <div>
+              <dt>Email</dt>
+              <dd>cody@krselectric.com</dd>
+            </div>
+            <div>
+              <dt>LinkedIn</dt>
+              <dd>
+                <a href="https://www.linkedin.com/in/cody-jackson-662874191/" target="_blank" rel="noreferrer">
+                  linkedin.com/in/cody-jackson-662874191/
+                </a>
+              </dd>
+            </div>
+          </dl>
+        </aside>
+
+        <div className="titles">
+          <h1>Cody Jackson</h1>
+          <h2>Software Developer</h2>
+        </div>
+        <div><h2></h2></div>
       </section>
-    </>
+    </main>
   )
 }
 
